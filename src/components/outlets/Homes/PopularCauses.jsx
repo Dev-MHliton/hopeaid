7be784255cae51c1d2804f237fd082b5c7646ebe
Dev-MHliton <liton41126nk@gmail.com>
2026-06-23 21:56:31 +0600

    set popular

--- src/components/outlets/Homes/PopularCauses.jsx
+++ src/components/outlets/Homes/PopularCauses.jsx
@@ -17,7 +17,7 @@ const PopularCauses = () => {
 
             <div
                 id="Causes"
-                className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6"
+                className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-12"
             >
                 {causes.map(cause => {
                     const percentage = Math.round(
