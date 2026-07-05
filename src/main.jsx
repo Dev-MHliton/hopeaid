322a7edbe9238f783e265d50b0ebbc88a69f9259
Dev-MHliton <liton41126nk@gmail.com>
2026-07-06 01:40:18 +0600

    authprovider temporary out

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import { RouterProvider } from 'react-router-dom'
 import { Router } from './Router/GeneralRoutes/Router'
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import AuthProvider from './providers/AuthProvider'
 
 const queryClient = new QueryClient();
 
@@ -14,9 +13,8 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* From api server data manage just only QueryClientProvider */}
     <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <RouterProvider router={Router} />
-      </AuthProvider>
+
+      <RouterProvider router={Router} />
     </QueryClientProvider>
   </StrictMode>,
 )
