bcecbac6d7e19660bfa1bfd9d5cb03696b059318
Dev-MHliton <liton41126nk@gmail.com>
2026-07-14 23:31:59 +0600

    navbar

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -3,6 +3,8 @@ const Footer = () => {
     return (
         <section>
             <footer className="footer footer-horizontal footer-center bg-base-200 text-base-content rounded p-10">
+
+                {/* Navbar  */}
                 <nav className="grid grid-flow-col gap-4">
                     <a href="about" className="link link-hover">About us</a>
                     <a href="contact" className="link link-hover">Contact</a>
